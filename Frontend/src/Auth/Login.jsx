64c91c59e05eb28f1e7daadf00d1f61bf60bf6d0
Jaisika30 <jaisika@antheminfotech.com>
2025-05-22 15:16:24 +0530

    up

--- Frontend/src/Auth/Login.jsx
+++ Frontend/src/Auth/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const BASE_URL = process.env.REACT_APP_API_URL;
-
+  console.log("baseeeeeeeeeeeeeeee urlllllllllllllll:::", BASE_URL)
   // const token = localStorage.getItem("token");
   const fetchCartList = async (token) => {
     console.log("hello jass");
